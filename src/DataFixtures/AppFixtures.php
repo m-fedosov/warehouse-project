4dde124977dcd09c880cc402abca8ec45758ee42
Mikhail Fedosov <mikhail.fedosov@vk.team>
2024-06-01 11:45:45 +0300

    FakerFactory for AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,49 +7,48 @@ use App\Entity\Warehouse;
 use App\Entity\WarehouseProduct;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // Создание складов
-        $warehouse1 = new Warehouse();
-        $warehouse1->setName('Склад 1');
-        $warehouse1->setIsAvailable(true);
-        $manager->persist($warehouse1);
+        $faker = Factory::create();
 
-        $warehouse2 = new Warehouse();
-        $warehouse2->setName('Склад 2');
-        $warehouse2->setIsAvailable(false);
-        $manager->persist($warehouse2);
+        // Создание складов
+        $warehouses = [];
+        for ($i = 0; $i < 10; $i++) {
+            $warehouse = new Warehouse();
+            $warehouse->setName('Склад ' . ($i + 1));
+            $warehouse->setIsAvailable($faker->boolean);
+            $manager->persist($warehouse);
+            $warehouses[] = $warehouse;
+        }
 
         // Создание товаров
-        $product1 = new Product();
-        $product1->setName('Товар 1');
-        $product1->setSize('Маленький');
-        $product1->setUniqueCode('P001');
-        $manager->persist($product1);
-
-        $product2 = new Product();
-        $product2->setName('Товар 2');
-        $product2->setSize('Средний');
-        $product2->setUniqueCode('P002');
-        $manager->persist($product2);
+        $products = [];
+        for ($i = 0; $i < 100; $i++) {
+            $product = new Product();
+            $product->setName('Товар ' . ($i + 1));
+            $product->setSize($faker->randomElement(['Маленький', 'Средний', 'Большой']));
+            $product->setUniqueCode('P' . str_pad($i + 1, 3, '0', STR_PAD_LEFT));
+            $manager->persist($product);
+            $products[] = $product;
+        }
 
         // Связь складов и товаров
-        $warehouseProduct1 = new WarehouseProduct();
-        $warehouseProduct1->setWarehouse($warehouse1);
-        $warehouseProduct1->setProduct($product1);
-        $warehouseProduct1->setQuantity(50);
-        $warehouseProduct1->setReservedQuantity(0);
-        $manager->persist($warehouseProduct1);
-
-        $warehouseProduct2 = new WarehouseProduct();
-        $warehouseProduct2->setWarehouse($warehouse2);
-        $warehouseProduct2->setProduct($product2);
-        $warehouseProduct2->setQuantity(150);
-        $warehouseProduct2->setReservedQuantity(0);
-        $manager->persist($warehouseProduct2);
+        foreach ($warehouses as $warehouse) {
+            foreach ($products as $product) {
+                if ($faker->boolean(50)) {
+                    $warehouseProduct = new WarehouseProduct();
+                    $warehouseProduct->setWarehouse($warehouse);
+                    $warehouseProduct->setProduct($product);
+                    $warehouseProduct->setQuantity($faker->numberBetween(1, 100));
+                    $warehouseProduct->setReservedQuantity($faker->numberBetween(0, 50));
+                    $manager->persist($warehouseProduct);
+                }
+            }
+        }
 
         // Сохранение всех данных в базу
         $manager->flush();
